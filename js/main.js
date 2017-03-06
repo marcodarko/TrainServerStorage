a1e71a677ff2f7c37a4fc2d247ff6342ec109df3
marco alvarado <Marco@marcos-MacBook-Pro.local>
2017-03-05 23:34:19 -0800

    interface done

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCUa3OmzBQAV9MHxQg6Pgl2s5533V5qjEI",
+  authDomain: "coder-bay-fee9d.firebaseapp.com",
+  databaseURL: "https://coder-bay-fee9d.firebaseio.com",
+  storageBucket: "coder-bay-fee9d.appspot.com"
+};
+
+firebase.initializeApp(config);
+
+// Create a variable to reference the database
+var database = firebase.database();
+
+// Initial Values
+var initialBid = 0;
+var initialBidder = "No one :-(";
+var highPrice = initialBid;
+var highBidder = initialBidder;
+
+// --------------------------------------------------------------
+
+// At the initial load, get a snapshot of the current data.
+database.ref().on("value", function(snapshot) {
+
+
+
+
+
+
+	
+});
